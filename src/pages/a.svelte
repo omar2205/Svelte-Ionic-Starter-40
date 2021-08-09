45fde375e1ee90343a13ca5328688ca67f0677f7
omar2205 <omar2205a@yahoo.com>
2021-08-09 18:21:32 +0200

    update bat

---
+++ src/pages/a.svelte
@@ -0,0 +1,23 @@
+<script>
+import { Device } from '@capacitor/device';
+import {onMount} from 'svelte'
+const logDeviceInfo = async () => {
+  const info = await Device.getInfo();
+
+  console.log(info);
+};
+
+let info
+const logBatteryInfo = async () => {
+  info = await Device.getBatteryInfo();
+
+  console.log(info);
+};
+onMount(() => {
+  logDeviceInfo()
+  logBatteryInfo()
+})
+</script>
+<div class="main">
+<h1>B: {info}</h1>
+</div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,35 +1,14 @@
 <script>
   import { IonicShowModal } from "@utils/IonicHelper";
   import Modal from "@components/Modal.wc.svelte"
-  // import * as X from '@ionic-native/battery-status'
-  import {BatteryStatus} from '@ionic-native/battery-status'
-  
-  import { Plugins } from '@capacitor/core'
-  const { Device } = Plugins
-  
-  let bat = '0%'
-  let bat2 = '0%'
-  let b, c
-  
+  import { Device } from '@capacitor/device'
   import {onMount} from 'svelte'
-  const loadInfo = async () => {
-    b = await Device.getBatteryInfo()
-    c = await Device.getInfo()
+  let bat
+  const logBatteryInfo = async () => {
+    bat = await Device.getBatteryInfo()
   }
   onMount(() => {
-    loadInfo()
-    // console.log(X)
-    // setTimeout(() => 
-    // , 1000)
-    window.BatteryStatus = BatteryStatus
-    BatteryStatus.onChange = e => {
-      bat = `${e.level}%`
-    }
-    window.addEventListener("batterystatus", onBatteryStatus, false);
-
-    function onBatteryStatus(e) {
-      bat2 = `${e.level}%`
-    }
+    logBatteryInfo()
   })
   const showModal = () => {
     IonicShowModal("modal-extra", Modal, {
@@ -42,11 +21,7 @@
 
 <div class="main">
   <h5>Welcome to <br/><br/><h3>Svelte with <br/>Ionic + Capacitor!</h3></h5>
-  <h1><pre>BAT    {bat}</pre></h1>
-  <h1><pre>BAT2   {bat}</pre></h1>
-  <h1><pre>XXB    
-  {JSON.stringify(b, null, 2)}
-  {JSON.stringify(c, null, 2)}</pre></h1>
+  <h1><pre>BAT    {bat * 100}%</pre></h1>
   <ion-button on:click={showModal}>Open Modal</ion-button>
 </div>
 
