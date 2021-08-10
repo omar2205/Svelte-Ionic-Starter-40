154800914b4eb32bde56eacebc38f042f1706bf4
omar2205 <omar2205a@yahoo.com>
2021-08-10 06:45:27 +0200

    demo2

--- src/pages/a.svelte
+++ src/pages/a.svelte
@@ -1,13 +1,21 @@
 <script>
   import { BackgroundMode } from "@ionic-native/background-mode";
   import { onMount } from "svelte";
+  let i = 0;
   onMount(() => {
     console.log(BackgroundMode);
     BackgroundMode.enable(true);
+    setInterval(() => {
+      i++;
+    }, 1000);
+    setInterval(() => {
+      if (i > 20 && i < 25) BackgroundMode.moveToForeground();
+    }, 500);
     console.log(BackgroundMode);
   });
 </script>
 <div class="main">
   <h1>Background mode</h1>
+  {i}<br/>
   <pre>{JSON.stringify(BackgroundMode.isActive(), null, 2)}</pre>
 </div>
