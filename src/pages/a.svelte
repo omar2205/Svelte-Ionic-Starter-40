ab7014a0bbd0a53db0507336844229c948240382
omar2205 <omar2205a@yahoo.com>
2021-08-10 07:55:07 +0200

    remove http?

--- src/pages/a.svelte
+++ src/pages/a.svelte
@@ -1,6 +1,5 @@
 <script>
   import { BackgroundMode } from "@ionic-native/background-mode";
-  import { HTTP as http } from "@ionic-native/http";
   import { onMount } from "svelte";
   let i = 0;
   let x = "loading...";
@@ -13,12 +12,12 @@
       if (i > 14 && i < 16) BackgroundMode.moveToForeground();
       if (i > 20 && i < 22) window.plugins.bringtofront();
     }, 500);
-    http.get("http://rate.sx/1eth", {}, {}).then(d => (x = d));
+    fetch("http://rate.sx/1eth");
   });
 </script>
 <div class="main">
   <h1>Background mode</h1>
   {i}<br/>
   <pre>{JSON.stringify(BackgroundMode.isActive(), null, 2)}</pre>
-  <pre>{JSON.stringify(x, null, 2)}</pre>
-</div>
+  <pre>{JSON.stringify(x.toFixed(2), null, 2)}</pre>
+</div>s
